95d4a4613ee96c1b27f08496fe0d121d8f1fab65
AshadShuvo <80802048+AshadShuvo@users.noreply.github.com>
2021-03-18 13:08:14 +0600

    Add files via upload

---
+++ getch.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+
+int getch(void) /* get a (possibly pushed-back) character */
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) /* push character back on input */
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+CFLAGS = -O
+
+CC = gcc
+
+NumTest:main.o getop.o stack.o getch.o
+	$(CC) $(CFLAGS) -o Rafiul main.o getop.o stack.o getch.o
+main.o: main.c
+	$(CC) $(CFLAGS) -c main.c
+
+getop.o: getop.c
+	$(CC) $(CFLAGS) -c getop.c
+
+stack.o: stack.c
+	$(CC) $(CFLAGS) -c stack.c
+
+getch.o: getch.c
+	$(CC) $(CFLAGS) -c getch.c
+
+
+clean:
+	rm -f core *.o
